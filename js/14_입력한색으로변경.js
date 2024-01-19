317eded884e7bf3b2c52c2abf68f5533b1bb997f
caftu0428 <carcar0428@naver.com>
2024-01-19 17:49:46 +0900

    자바

--- js/14_입력한색으로변경.js
+++ js/14_입력한색으로변경.js
@@ -1,22 +1,32 @@
+
+// 요소를 한 번에 여러개 얻어오면 배열 형태로 저장된다!
+// -> 얻어온 요소를 사용하려면 배열의 index별 요소를 하나씩 꺼내서
+//    사용해야 한다!!
+
+// ex) boxList.style.color = "red";  // 배열의 글자색을 red 변경 (X)
+//    boxList[0].style.color = "blue"; // 0번 인덱스 요소 글자색 blue (O)
+
 const boxList = document.getElementsByClassName("box");
 const inputList = document.getElementsByClassName("color-input");
 
-function changeColor(){
-  for(let i=0 ; i <inputList.length ; i++){
-    boxList[i].style.backgroundColor = inputList[i].value;
-  }
-}
-
-//--------------------------
-
-inputList.addEventListener("keyup",function(e){
-
-    if(e.key == "Enter"){
-        boxList.style.backgroundColor = inputList.value;
+document.querySelector("#changeButton")
+  .addEventListener("click", function(){
+    for(let i=0 ; i <inputList.length ; i++){
+      boxList[i].style.backgroundColor = inputList[i].value;
     }
-});
+  });
 
-boxList.addEventListener("click", function(e){
 
-    alert(`배경색 : ${e.target.style.backgroundColor}`);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
